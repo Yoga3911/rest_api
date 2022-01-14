894f5734dca02116f5d00a7531bf978d9b10768c
Yoga3911 <yoga3911@gmail.com>
2022-01-14 11:38:33 +0700

    add goroutine

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"rest_api/routes"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -11,12 +12,27 @@ import (
 //tes
 func main() {
 	defer routes.DB.Close()
+
+	ticker := time.NewTicker(30 * time.Millisecond)
+	quit := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				fmt.Println("Hello master i'm still awake")
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+	
 	app := fiber.New(fiber.Config{
 		Prefork:           true,
 		StreamRequestBody: true,
 	})
 	routes.Route(app)
 	p := os.Getenv("PORT")
-	p = fmt.Sprintf(":%v",p)
+	p = fmt.Sprintf(":%v", p)
 	app.Listen(p)
 }
