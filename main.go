11b40db9ece76c1dd1589076621fcba19c1c9b8b
Yoga3911 <yoga3911@gmail.com>
2022-01-14 13:53:11 +0700

    remove unused goroutine

--- main.go
+++ main.go
@@ -5,34 +5,20 @@ import (
 	"os"
 	"rest_api/routes"
 	"time"
-
 	"github.com/gofiber/fiber/v2"
 )
 
-//tes
 func main() {
 	defer routes.DB.Close()
-
-	ticker := time.NewTicker(29 * time.Minute)
-	quit := make(chan struct{})
-	go func() {
-		for {
-			select {
-			case <-ticker.C:
-				fmt.Println("Hello master i'm still awake")
-			case <-quit:
-				ticker.Stop()
-				return
-			}
-		}
-	}()
 	
 	app := fiber.New(fiber.Config{
 		Prefork:           true,
 		StreamRequestBody: true,
 	})
+
 	routes.Route(app)
 	p := os.Getenv("PORT")
 	p = fmt.Sprintf(":%v", p)
+
 	app.Listen(p)
 }
