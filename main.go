abcac17fa47c7ec5f1f2c6dbb8ea5d078216320d
Yoga3911 <yoga3911@gmail.com>
2022-01-17 10:11:32 +0700

    small changes

--- main.go
+++ main.go
@@ -11,13 +11,22 @@ import (
 func main() {
 	defer routes.DB.Close()
 
+	app := fiber.New(fiber.Config{
+		Prefork:           true,
+		StreamRequestBody: true,
+	})
+
 	ticker := time.NewTicker(25 * time.Minute)
 	quit := make(chan struct{})
 	go func() {
 		for {
 			select {
 			case <-ticker.C:
-				fiber.Get("/")
+				app.Get("/", func(c *fiber.Ctx) error {
+					return c.JSON(fiber.Map{
+						"message": "OK!",
+					})
+				})
 			case <-quit:
 				ticker.Stop()
 				return
@@ -25,14 +34,9 @@ func main() {
 		}
 	}()
 
-	app := fiber.New(fiber.Config{
-		Prefork:           true,
-		StreamRequestBody: true,
-	})
-	
 	routes.Route(app)
 	p := os.Getenv("PORT")
-	p = fmt.Sprintf(":%v",p)
-	
+	p = fmt.Sprintf(":%v", p)
+
 	app.Listen(p)
 }
