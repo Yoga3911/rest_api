1e6ef3b7d122190a4855ec972863d56b0f23eded
Yoga3911 <yoga3911@gmail.com>
2022-01-17 10:05:59 +0700

    change database url

--- config/db-config.go
+++ config/db-config.go
@@ -2,7 +2,7 @@ package config
 
 import (
 	"context"
-	"fmt"
+	// "fmt"
 	"github.com/jackc/pgx/v4/pgxpool"
 	// "github.com/joho/godotenv"
 	"os"
@@ -14,14 +14,14 @@ func DatabaseConnection() *pgxpool.Pool {
 	// 	panic(err)
 	// }
 
-	dbHost := os.Getenv("DB_HOST")
-	dbName := os.Getenv("DB_NAME")
-	dbUser := os.Getenv("DB_USER")
-	dbPass := os.Getenv("DB_PASSWORD")
-	dbPort := os.Getenv("DB_PORT")
-	dsn := fmt.Sprintf("host=%s user=%s password=%s port=%s dbname=%s sslmode=require TimeZone=Asia/Jakarta",dbHost, dbUser, dbPass, dbPort, dbName)
-
-	pgx, err := pgxpool.Connect(context.Background(), dsn)
+	// dbHost := os.Getenv("DB_HOST")
+	// dbName := os.Getenv("DB_NAME")
+	// dbUser := os.Getenv("DB_USER")
+	// dbPass := os.Getenv("DB_PASSWORD")
+	// dbPort := os.Getenv("DB_PORT")
+	// dsn := fmt.Sprintf("host=%s user=%s password=%s port=%s dbname=%s sslmode=require TimeZone=Asia/Jakarta",dbHost, dbUser, dbPass, dbPort, dbName)
+	// dsn := fmt.Sprintln()
+	pgx, err := pgxpool.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		panic(err)
 	}
